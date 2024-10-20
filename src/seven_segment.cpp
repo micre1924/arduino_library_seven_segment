0445ab2d78a42d3c2f5683c600f236a5ef6cf79f
micre1924 <michaelreul87@gmail.com>
2024-10-20 23:44:51 +0200

    sync_acb

--- src/seven_segment.cpp
+++ src/seven_segment.cpp
@@ -3,15 +3,14 @@
 
 namespace mrc{
 
-	seven_segment::seven_segment(uint8_t *seg_pins, uint8_t *digit_pins, uint8_t digit_amount, bool seg_low, int digit_sustain_time, uint8_t *return_pins, uint8_t return_length)
+	seven_segment::seven_segment(const int8_t *seg_pins, const int8_t *digit_pins, const uint8_t digit_amount, const bool seg_low, const int digit_sustain_time, const int8_t *return_pins, const uint8_t return_length)
+	: 	seg_pins(seg_pins), digit_pins(digit_pins), digit_amount(digit_amount), 
+		seg_low(seg_low), digit_sustain_time(digit_sustain_time), return_pins(return_pins), 
+		return_length(return_length), is_shift_reg(false),
+		// not needed variable initialization
+		shift_pin(-1), latch_pin(-1), serial_pin(-1),
+		segs_on_end(false)
 	{
-		seven_segment::return_pins = return_pins;
-		seven_segment::seg_pins = seg_pins;
-		seven_segment::digit_pins = digit_pins;
-		seven_segment::digit_amount = digit_amount;
-		seven_segment::return_length = return_length;
-		seven_segment::seg_low = seg_low;
-		seven_segment::is_shift_reg = false;
 
 		seven_segment::draw_buffer = new uint8_t[seven_segment::digit_amount + 1];
 
@@ -31,17 +30,15 @@ namespace mrc{
 		}
 
 	}
-	seven_segment::seven_segment(uint8_t shift_pin, uint8_t latch_pin, uint8_t serial_pin, uint8_t digit_amount, bool seg_low, bool segs_on_end, uint8_t *return_pins, uint8_t return_length) 
+	
+	seven_segment::seven_segment(const int8_t shift_pin, const int8_t latch_pin, const int8_t serial_pin, const uint8_t digit_amount, const bool seg_low, const bool segs_on_end, const int8_t *return_pins, const uint8_t return_length) 
+	: 	shift_pin(shift_pin), latch_pin(latch_pin), serial_pin(serial_pin), 
+		digit_amount(digit_amount), return_pins(return_pins), seg_low(seg_low), 
+		segs_on_end(segs_on_end), return_length(return_length), is_shift_reg(true),
+		// not needed variable initialization
+		seg_pins((int8_t*)-1), digit_pins((int8_t*)-1), 
+		digit_sustain_time(0)
 	{
-		seven_segment::return_pins = return_pins;
-		seven_segment::shift_pin = shift_pin;
-		seven_segment::latch_pin = latch_pin;
-		seven_segment::serial_pin = serial_pin;
-		seven_segment::segs_on_end = segs_on_end;
-		seven_segment::digit_amount = digit_amount;
-		seven_segment::return_length = return_length;
-		seven_segment::seg_low = seg_low;
-		seven_segment::is_shift_reg = true;
 
 		seven_segment::draw_buffer = new uint8_t[seven_segment::digit_amount + 1];
 
@@ -61,60 +58,84 @@ namespace mrc{
 
 	void seven_segment::draw() {
 		uint8_t last_d_inv = 0U;
-		for (uint8_t d = 0; d < seven_segment::digit_amount; d++) {
-			uint8_t d_inv = (seven_segment::digit_amount - 1) - d;
-			if (seven_segment::is_shift_reg) {
-				long position = 1 << d;
-				for (uint8_t i = seven_segment::digit_amount - 1; i >= 0; i--) {
-					digitalWrite(seven_segment::serial_pin, ((position >> i) % 2) == seven_segment::seg_low);
-					digitalWrite(seven_segment::shift_pin, HIGH);
-					digitalWrite(seven_segment::shift_pin, LOW);
-				}
-			}
-			else {
-				digitalWrite(seven_segment::digit_pins[last_d_inv], !seven_segment::seg_low);
-			}
+		
+		// current drawing position
+		if (seven_segment::current_digit_draw_position >= seven_segment::digit_amount - 1)
+			seven_segment::current_digit_draw_position = 0; 
+		else seven_segment::current_digit_draw_position++;
 
-			for (int8_t s = 7; s >= 0; s--) {
-				bool set_bit = (seven_segment::draw_buffer[d] >> s) % 2;
-				if (seven_segment::is_shift_reg) {
-					digitalWrite(seven_segment::serial_pin, set_bit == !seven_segment::seg_low);
-					digitalWrite(seven_segment::shift_pin, HIGH);
-					digitalWrite(seven_segment::shift_pin, LOW);
-				}
-				else digitalWrite(seven_segment::seg_pins[s], set_bit == !seven_segment::seg_low);
-			}
-			if (seven_segment::is_shift_reg) {
-				digitalWrite(seven_segment::latch_pin, HIGH);
-				digitalWrite(seven_segment::latch_pin, LOW);
-			}
-			else {
-				digitalWrite(seven_segment::digit_pins[d_inv], seven_segment::seg_low);
-				delayMicroseconds(100);
-				last_d_inv = d_inv;
-			}
-			for (uint8_t r = 0; r < seven_segment::return_length; r++) {
-				seven_segment::return_buffer[r][d] = digitalRead(seven_segment::return_pins[r]) == seven_segment::seg_low;
+		// digit position declare
+		uint8_t d = seven_segment::current_digit_draw_position;
+		uint8_t d_inv = (seven_segment::digit_amount - 1) - d;
+
+		// set digit for draw
+		if (seven_segment::is_shift_reg) {
+			// Shift Register Mode
+			long position = 1 << d;
+			for (int8_t i = seven_segment::digit_amount - 1; i >= 0; i--) {
+				digitalWrite(seven_segment::serial_pin, ((position >> i) % 2) == seven_segment::seg_low);
+				digitalWrite(seven_segment::shift_pin, HIGH);
+				digitalWrite(seven_segment::shift_pin, LOW);
 			}
 		}
+		else {
+			// Normal Wired Mode
+			digitalWrite(seven_segment::digit_pins[last_d_inv], !seven_segment::seg_low);
+		}
+
+		// set segments for draw
+		for (int8_t s = 7; s >= 0; s--) {
+			// set seg Date bit
+			bool set_bit = (seven_segment::draw_buffer[d] >> s) % 2;
+			if (seven_segment::is_shift_reg) {
+				// Shift Register Mode
+				digitalWrite(seven_segment::serial_pin, set_bit == !seven_segment::seg_low);
+				digitalWrite(seven_segment::shift_pin, HIGH);
+				digitalWrite(seven_segment::shift_pin, LOW);
+			}
+			else {
+				// Normal Wired Mode
+				digitalWrite(seven_segment::seg_pins[s], set_bit == !seven_segment::seg_low);
+			}
+		}
+
+		// Latch/Sustain Output
+		if (seven_segment::is_shift_reg) {
+			// Shift Register Mode
+			digitalWrite(seven_segment::latch_pin, HIGH);
+			digitalWrite(seven_segment::latch_pin, LOW);
+		}
+		else {
+			// Normal Wired Mode
+			digitalWrite(seven_segment::digit_pins[d_inv], seven_segment::seg_low);
+			delayMicroseconds(seven_segment::digit_sustain_time);
+			last_d_inv = d_inv;
+		}
+
+		// read pins for aditional Key Matrix
+		for (uint8_t r = 0; r < seven_segment::return_length; r++) {
+			seven_segment::return_buffer[r][d] = digitalRead(seven_segment::return_pins[r]) == seven_segment::seg_low;
+		}
+		
+		// Normal Wired Mode - Cut Output for current Digit
 		if(!is_shift_reg) digitalWrite(seven_segment::digit_pins[last_d_inv], !seven_segment::seg_low);
+		
 	}
 
 	//drawables: .0123456789ABCDEFGHIJLNOPQRSUXY
-	void seven_segment::write(char* input) {
+	void seven_segment::write(const char* input) {
 		for (uint16_t c = 0, d = 0; d <= (seven_segment::digit_amount + 1); c++, d++) {
+
 			seven_segment::draw_buffer[d] = 0;
-			switch (input[c])
+
+			for (size_t s = 0; s < seven_segment::symbolic_length; s++)
 			{
-				//case 0xDF: draw_buffer[d] = 99; break;	//�
-				//case 0xE4: draw_buffer[d] = 114; break;	//�
-				case '-': draw_buffer[d] = 64; break;
-				case '(': draw_buffer[d] = 57; break;
-				case ')': draw_buffer[d] = 15; break;
-				case '=': draw_buffer[d] = 72; break;
-				case 0x22: draw_buffer[d] = 34; break;	//"
-				default: break;
+				if(seven_segment::symbolic_adaptor[s] == input[c]){
+					draw_buffer[d] = seven_segment::symbolic_codec[s];
+					break;
+				}
 			}
+
 			if (input[c] >= 65 && input[c] <= 90) draw_buffer[d] = seven_segment::letters_codec[input[c] - 65];	//ascii A..Z
 			if (input[c] >= 48 && input[c] <= 57) draw_buffer[d] = seven_segment::numbers_codec[input[c] - 48];	//ascii 0..9 to seven_seg
 			if (input[c] == 46 || input[c] == 44) { 
--- src/seven_segment.h
+++ src/seven_segment.h
@@ -4,50 +4,96 @@ namespace mrc{
 
 	class seven_segment {
 	public:
-		//Native-Arduino-Pin-Mode
-		//if used, Pin-Mode is already set
 		/**
+		 * @brief Native-Arduino-Pin-Mode ( if used, Pin-Mode is already set )
 		 * @param seg_pins Segement Pins as an uint8_t Array
 		 * @param digit_pins Pins of every Digit on the Entirety of the Display as an uint8_t Array
 		 * @param digit_amount Number of Digits on the Entirety of the Display as Int
 		 * @param seg_low true = low, false = high
 		 * @param digit_sustain_time time that a digit is sustained in microseconds
+		 * @param return_pins Return Pins for additional Key Matrix
+		 * @param return_length Return Length of the additional Key Matrix
 		 */
-		seven_segment(uint8_t *seg_pins, uint8_t *digit_pins, uint8_t digit_amount, bool seg_low, int digit_sustain_time = 100, uint8_t *return_pins = {}, uint8_t return_length = 0);
-		//Shift-Register-Mode
-		//if used, Pin-Mode is already set
-		seven_segment(uint8_t shift_pin, uint8_t latch_pin, uint8_t serial_pin, uint8_t digit_amount, bool seg_low, bool segs_on_end, uint8_t *return_pins = {}, uint8_t return_length = 0);
-		//pointers
-		bool 	**return_buffer;
-		bool	seg_low,
-				is_shift_reg,
-				segs_on_end;
-		uint8_t *seg_pins, 
-				*digit_pins, 
-				*return_pins,
-				digit_amount,
-			 	*draw_buffer,
-				shift_pin, 
-				latch_pin, 
-				serial_pin, 
-				return_length;
-		//normal
-		uint8_t dim_value = 10;
-		int 	digit_sustain_time;
-		//0123456789
-		uint8_t	numbers_codec[10] = { 63,6,91,79,102,109,125,7,127,111 };
+		seven_segment(const int8_t *seg_pins, const int8_t *digit_pins, const uint8_t digit_amount, const bool seg_low, const int digit_sustain_time = 100, const int8_t *return_pins = {}, const uint8_t return_length = 0);
+		
+		/**
+		 * @brief Shift-Register-Mode ( if used, Pin-Mode is already set )
+		 * @param shift_pin Shift Pin of Shift Register
+		 * @param latch_pin Latch Pin of Shift Register
+		 * @param serial_pin Serial Pin of Shift Register
+		 * @param digit_amount Number of Digits on the Entirety of the Display as Int
+		 * @param seg_low true = low, false = high
+		 * @param seg_on_end Segment Pins are on the End of the Shift Register
+		 * @param return_pins Pins for receiving parrallel data from additional Key Matrix
+		 * @param return_length Return Length of the additional Key Matrix
+		 */
+		seven_segment(const int8_t shift_pin, const int8_t latch_pin, const int8_t serial_pin, const uint8_t digit_amount, const bool seg_low, const bool segs_on_end, const int8_t *return_pins = {}, const uint8_t return_length = 0);
+		
+		// ----- Normal Wired exclusive variables -----
 
-		//ABCDEFGHIJLNOPQRSTUXY
-		uint8_t letters_codec[26] = { 119,124,57,94,121,113,111,118,6,30,112,56,84,84,92,115,103,80,109,120,42,42,42,118,114,91 };
+			// Normal Wired Segment-Pins
+			const int8_t 		*seg_pins;
+			// Normal Wired Segment-Pins
+			const int8_t		*digit_pins;
 
-		//��-()
-		//char symbolic_adaptor[7] = { '�', '�', '-', '(', ')', '=', '"' };
-		//char symbolic_codec[7] = { 99,114,64,57,15,82,34 };
+		// ---- Shift Register exclusive variables ----
 
-		//draws the written draw-buffer once
-		//returns 2D Array input-matrix [return-value][mux-position]
-		void draw();
-		void write(char* input);
+			// Shift Register Shift Pin
+			const uint8_t		shift_pin; 
+			// Shift Register Latch Pin
+			const uint8_t		latch_pin;
+			// Shift Register Serial Pin
+			const uint8_t		serial_pin;
+
+			// Shift Register Segment pins location
+			const bool 			segs_on_end;
+
+		// ----- Variables needed for both Modes ------
+
+			// Return Mode (Additional Key Matrix)
+			
+				// Pins for receiving parrallel data from additional Key Matrix
+				const int8_t 	*return_pins;
+				// Buffer for returned data from additional Key Matrix
+				bool 			**return_buffer;
+				// Length of parrallel retured Keymatrix Data in Bits
+				uint8_t			return_length;
+			
+			// Segments are Low-Active
+			const bool			seg_low;
+
+			// initialized Mode (Wired Normal or Shift Register)
+			const bool			is_shift_reg;
+
+			// 7-Segment Raw Draw buffer					
+			uint8_t 			*draw_buffer;
+
+			// Current Segment Drawing Position
+			uint8_t				current_digit_draw_position;
+			
+			// Amount of 7-Segment Digits
+			const uint8_t		digit_amount;
+
+			//normal
+			uint8_t 			dim_value = 10;
+			const int 			digit_sustain_time;
+
+			//0123456789
+			const uint8_t		numbers_codec[10] = { 63,6,91,79,102,109,125,7,127,111 };
+
+			//ABCDEFGHIJLNOPQRSTUXY
+			const uint8_t 		letters_codec[26] = { 119,124,57,94,121,113,111,118,6,30,112,56,84,84,92,115,103,80,109,120,42,42,42,118,114,91 };
+
+			//special Characters
+			const uint8_t 		symbolic_length = 6;
+			const char 			symbolic_adaptor[7] = { '-', '(', ')', '=', '"', '*' };
+			const uint8_t 		symbolic_codec[7] = { 64, 57, 15, 82, 34, 99 };
+
+			//draws one digit of the written draw-buffer
+			void draw();
+
+			// write cstring to Buffer
+			void write(const char* input);
 
 	};
 }
